d313f8615f61693c1eeca89dca1a829395428e57
rondrae <rondrae@gmail.com>
2023-07-15 15:11:03 -0400

    added LB config

---
+++ loadbalancing/variables.tf
@@ -0,0 +1,28 @@
+variable "public_sg" {
+
+}
+
+variable "public_subnets" {
+
+}
+variable "tg_port" {
+
+}
+variable "tg_protocol" {
+
+}
+variable "lb_healthy_threshold" {
+
+}
+variable "lb_unhealthy_threshold" {
+
+}
+variable "lb_timeout" {
+
+}
+variable "lb_interval" {
+
+}
+variable "vpc_id" {
+
+}
--- main.tf
+++ main.tf
@@ -14,16 +14,31 @@ module "networking" {
   db_subnet_group  = true
 }
 
-module "database" {
-  source                 = "./database"
-  db_storage             = 10
-  db_engine_version      = "5.7"
-  db_instance_class      = "db.t2.micro"
-  dbname                 = var.dbname
-  dbuser                 = var.dbuser
-  dbpassword             = var.dbpassword
-  db_identifier          = "mtc-db"
-  skip_db_snapshot       = true
-  db_subnet_group_name   = module.networking.db_subnet_group_name[0]
-  vpc_security_group_ids = module.networking.db_security_group
+# module "database" {
+#   source                 = "./database"
+#   db_storage             = 10
+#   db_engine_version      = "5.7"
+#   db_instance_class      = "db.t2.micro"
+#   dbname                 = var.dbname
+#   dbuser                 = var.dbuser
+#   dbpassword             = var.dbpassword
+#   db_identifier          = "mtc-db"
+#   skip_db_snapshot       = true
+#   db_subnet_group_name   = module.networking.db_subnet_group_name[0]
+#   vpc_security_group_ids = module.networking.db_security_group
+# }
+
+module "loadbalancing" {
+  source                 = "./loadbalancing"
+  public_sg              = module.networking.public_sg
+  public_subnets         = module.networking.public_subnets
+  vpc_id                 = module.networking.vpc_id
+  tg_port                = 80
+  tg_protocol            = "HTTP"
+  lb_healthy_threshold   = 2
+  lb_unhealthy_threshold = 2
+  lb_timeout             = 3
+  lb_interval            = 30
+
+
 }
